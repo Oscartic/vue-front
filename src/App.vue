3830df148476d7c193a208b5987774846c1682a6
OscarB <oscartic@gmail.com>
2019-10-19 17:38:05 -0300

    Menu Main app was added

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,197 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+  <v-app id="app">
+     <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+        </template>
+        <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>Almacén</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Categorias</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+             <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Artículos</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+         <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>Compras</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Ingresos</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+             <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Proveedores</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+
+        <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>Ventas</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Ventas</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+             <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Clientes</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+        <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>Accesos</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Usuarios</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+        <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>Consultas</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Consultas compras</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+             <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>Consultas Ventas</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+<!-- app bar -->
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+<!-- Toolbar -->
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-3"
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Sistema ORB</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
+      <v-btn icon>
+        <v-icon>logout</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+<!-- Router app -->
+      <v-container fluid fill-height>
+        <router-view/>
+      </v-container>
     </v-content>
+<!-- Footer -->
+    <v-footer height="auto">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+              ORBSW &copy; 2019
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<script> 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: null
+    }
+  }
 };
 </script>
